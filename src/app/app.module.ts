870897116c493ed443edebc8c8fa3a528526e26a
komura <affirman03139@outlook.jp>
2020-08-01 05:47:36 +0900

    feat(hello): helloでテスト

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent, HelloComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    customElements.define('app-root', createCustomElement(AppComponent, { injector: this.injector }));
+    customElements.define('app-hello', createCustomElement(HelloComponent, { injector: this.injector }));
+  }
+}
