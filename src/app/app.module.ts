e711e5e9fbcb29117c7a7b49188b57b7af779376
komura <affirman03139@outlook.jp>
2020-08-01 06:48:23 +0900

    fix(app): appコンポーネントを修正、routingを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 
@@ -13,17 +12,15 @@ import { HelloComponent } from './hello/hello.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   entryComponents: [AppComponent, HelloComponent]
 })
 export class AppModule {
   constructor(private injector: Injector) {
-  }
-
-  ngDoBootstrap() {
     customElements.define('app-root', createCustomElement(AppComponent, { injector: this.injector }));
     customElements.define('app-hello', createCustomElement(HelloComponent, { injector: this.injector }));
   }
+
+  ngDoBootstrap() { }
 }
