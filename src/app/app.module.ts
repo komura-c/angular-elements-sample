9e4aafab9b4841c598ed3cc021936fe493e39574
komura <affirman03139@outlook.jp>
2020-08-01 15:21:32 +0900

    feat(form): form追加とindex修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,24 @@ import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
   ],
   providers: [],
-  entryComponents: [AppComponent, HelloComponent]
+  entryComponents: [AppComponent, HelloComponent, FormComponent]
 })
 export class AppModule {
-  constructor(private injector: Injector) {
-    customElements.define('app-root', createCustomElement(AppComponent, { injector: this.injector }));
+  constructor(public injector: Injector) { }
+  ngDoBootstrap() {
     customElements.define('app-hello', createCustomElement(HelloComponent, { injector: this.injector }));
+    customElements.define('app-form', createCustomElement(FormComponent, { injector: this.injector }));
   }
-
-  ngDoBootstrap() { }
 }
